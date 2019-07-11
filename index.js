87e0090698e1bbb289fdd22980cd2e4bb1b44005
Kodziak <kodziak1416@gmail.com>
2019-07-11 23:53:57 +0200

    Implement cut string into two to bot handling messages.D

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const axios = require('axios')
 const client = new Discord.Client();
 const credentials = require('./credentials');
 
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
     const ARGS = msg.content.split(" ");
-    if (ARGS[0] === "/gif") {
-        msg.channel.send("doing request with: " + ARGS[1])
-        let url = "http://api.giphy.com/v1/gifs/translate?s=" + ARGS[1] + "&api_key=" + credentials.credentials.api_key + "&weirdness=10";
+    let firstWord = [];
+    let restWords = [];
+    for(let i = 0; i < ARGS.length; i++) {
+        if(i === 0) {
+            firstWord.push(ARGS[0])
+        } else {
+            restWords.push(ARGS[i])
+        }
+    }
+    firstWord = firstWord.join('');
+    restWords = restWords.join(' ');
+    let plusWords = restWords.replace(/ /g, "+");
+
+    if (firstWord === "/gif" && restWords.length > 0) {
+        let url = "http://api.giphy.com/v1/gifs/translate?s=" + plusWords + "&api_key=" + credentials.credentials.api_key + "&weirdness=10";
         axios.get(url).then(response => {
             msg.channel.send("Gif:", { file: response.data.data.images.fixed_height_downsampled.url });
         }).catch(error => {
