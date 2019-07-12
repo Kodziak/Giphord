2f74e7ca15a809dc29df59a7822b7ac39c1ba56b
Kodziak <kodziak1416@gmail.com>
2019-07-12 17:54:56 +0200

    Add help menu.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js');
 const axios = require('axios')
+const fs = require('fs')
 const client = new Discord.Client();
 const { token, api_key } = require('./credentials');
 const utils = require('./utils');
+let commandsList = fs.readFileSync('commands/help.txt', 'utf8')
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    let firstWord = utils.getFirstWord(msg.content);
+    let prefix = utils.getFirstWord(msg.content);
     let restWords = utils.getStringExceptFirstWord(msg.content);
     restWords = utils.replaceSpaceWithPlus(restWords);
 
@@ -17,13 +20,15 @@ client.on('message', msg => {
         msg.reply("You need to put a word!")
     }
 
-    if (firstWord === "/gif" && restWords) {
+    if (prefix === "/gif" && restWords) {
         let url = "http://api.giphy.com/v1/gifs/translate?s=" + restWords + "&api_key=" + api_key + "&weirdness=10";
         axios.get(url).then(response => {
             msg.channel.send("Gif:", { file: response.data.data.images.fixed_height_downsampled.url });
         }).catch(error => {
             msg.channel.send(error)
         })
+    } else if (prefix === "/gif-help") {
+        msg.channel.send(commandsList);
     }
 });
 
